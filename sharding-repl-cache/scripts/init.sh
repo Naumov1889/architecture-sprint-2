8aac27f2aad8d6967daadcf9e12fcdbd420591e1
maksim.naumov1889@gmail.com <maksim.naumov1889@gmail.com>
2025-03-13 21:06:00 +0300

    cache

--- sharding-repl-cache/scripts/init.sh
+++ sharding-repl-cache/scripts/init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Wait for mongo instances to be available
+sleep 10;
+
 # Инициализируйте сервер конфигурации:
 docker compose exec -T configSrv mongosh --port 27017 <<EOF
 rs.initiate(
@@ -7,7 +10,7 @@ rs.initiate(
     _id : "config_server",
        configsvr: true,
     members: [
-      { _id : 0, host : "configSrv:27017" }
+      {_id: 0, host: "configSrv:27017"}
     ]
   }
 );
@@ -19,7 +22,7 @@ rs.initiate(
     {
       _id : "shard1",
       members: [
-        { _id : 0, host : "shard1:27018" },
+        {_id: 0, host: "shard1:27018"},
         {_id: 1, host: "shard1-secondary-1:27021"},
         {_id: 2, host: "shard1-secondary-2:27022"},
       ]
@@ -33,7 +36,7 @@ rs.initiate(
     {
       _id : "shard2",
       members: [
-        { _id : 1, host : "shard2:27019" },
+        {_id: 0, host : "shard2:27019"},
         {_id: 1, host: "shard2-secondary-1:27023"},
         {_id: 2, host: "shard2-secondary-2:27024"},
       ]
